e239b2a4b7290244685f2dbe18f6b13a072af51b
krss-sys <kgm.naoya@gmail.com>
2026-02-27 18:23:33 +0700

    Completed lesson 1: Variables and Datatypes

---
+++ research/01_variable_datatype.py
@@ -0,0 +1,7 @@
+ten_bot = "Aoi"   #đây là str
+nam_tao = 2026    #đây là int
+la_AI = True      #đây là bool
+so_str = "100"
+print(f"Bot {ten_bot} duoc tao ra vao nam {nam_tao}. La AI:{la_AI}")
+print(f"Kết quả là:{int(so_str)+50}")  #ép giá trị tính toán
+print("Kết quả là:"+str(50+int(so_str)))
